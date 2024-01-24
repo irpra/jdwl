4afe8afb9a8dac26ebf1bb8d29ee736dc797d326
irpra <irpra@users.noreply.github.com>
2024-01-25 06:44:48 +0700

    Add files via upload

--- assets/js/baca-xml.js
+++ assets/js/baca-xml.js
@@ -7,9 +7,35 @@ document.addEventListener("DOMContentLoaded", () => {
       //console.log(data); //string
       let parser = new DOMParser();
       let xml = parser.parseFromString(data, "application/xml");
-      ambilMapel(xml);
-      ambilGuru(xml);
-      //  ambilHari(xml);
       lihatjadwalKelas(xml);
     });
 });
+
+function lihatjadwalKelas(x) {
+  //BACA LESSONIDCARD, PERIODCARD, DAYSCARD
+  let i = 0;
+  let tbody = document.createElement("tbody");
+
+  while (i < x.getElementsByTagName("card").length) {
+    let baris = document.createElement("tr");
+    let arrayCardAtt = [
+      i + 1,
+      x.getElementsByTagName("card")[i].getAttribute("lessonid"),
+      x.getElementsByTagName("card")[i].getAttribute("period"),
+      x.getElementsByTagName("card")[i].getAttribute("days"),
+    ];
+
+    for (let j = 0; j < arrayCardAtt.length; j++) {
+      let sel = document.createElement("td");
+      console.log(arrayCardAtt[j]);
+      let textSel = document.createTextNode(arrayCardAtt[j]);
+      sel.appendChild(textSel);
+      console.log(textSel);
+      baris.appendChild(sel);
+    }
+    ++i;
+
+    tbody.appendChild(baris);
+  }
+  document.getElementById("tbllihatjadwalKelas").appendChild(tbody);
+}
